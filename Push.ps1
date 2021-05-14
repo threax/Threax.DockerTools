037ffb8c86b6834f5e96aa133230f4f6519c6b0f
Andrew Piper <andrew@threax.com>
2021-05-14 09:16:35 -0400

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.DockerTools.git
+git push https://github.com/threax/Threax.DockerTools.git --tags
+git push https://gitlab.com/threax-software/Threax.DockerTools.git
+git push https://gitlab.com/threax-software/Threax.DockerTools.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.DockerTools
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.DockerTools --tags
